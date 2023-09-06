60412ff62a0fbb834291d1bd798371b399667e86
RuM <ryoutaasami1029@gmail.com>
2023-09-06 09:34:19 +0900

    up
    ジャンプ追加

--- Assets/code/CharController.cs
+++ Assets/code/CharController.cs
@@ -8,21 +8,34 @@ public class CharController : MonoBehaviour
 
     public float jumpForce = 200f; //ジャンプの高さ
 
-    public int MaxJumpcount = 2;
+    public int MaxJumpCount = 2;
     private int jumpcount = 0;
 
     public float speed= 0.05f; //キャラクターのスピード
     public int slow = 5;　//キャラクターのスピードのコントロール用
 
     //キャラクターコントローラー
-    void Update()
+    private void Start()
+    {
+        //RigidBody2Dコンポーネントの取得
+        rigidbody2D= GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
     {
         Vector2 position = transform.position;
 
-        if (Input.GetKey("w"))
+        //スペースを押したらジャンプさせる
+        //MaxJumpCountの数だけジャンプできる
+        if(Input.GetKey(KeyCode.Space) 
+            &&
+            this.jumpcount < MaxJumpCount)
         {
-            position.y += speed / slow;
+            this.rigidbody2D.AddForce(transform.up
+                *
+                jumpForce);
+            jumpcount++;
         }
+       
         if (Input.GetKey("s"))
         {
             position.y -= speed / slow;
@@ -37,6 +50,15 @@ public class CharController : MonoBehaviour
         }
 
         transform.position = position;
+    }
+
+    //床に着地したら、jumpCountを0にする
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            jumpcount = 0;
+        }
     }
 }
 
